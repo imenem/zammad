1e8080ceb3d332ddd7dc2e3244c3ef8c47559e06
Artem Ponomarenko <imenem@inbox.ru>
2020-03-24 18:20:58 +0200

    Disable telegram_update_puller initializer for database migration tasks.
    telegram_update_puller requires database to be migrated for start.
    It makes no sense to run it during migration.
    Even worse, it caused migration process to fail.

--- config/initializers/telegram_update_puller.rb
+++ config/initializers/telegram_update_puller.rb
@@ -1,3 +1,18 @@
+def migrating?
+  running_tasks = Rake.application.top_level_tasks || []
+  running_tasks.any? { |task_name| migration_task? task_name }
+end
+
+def migration_task?(task_name)
+  %w[db: zammad:db].any? { |migration_task_prefix| task_name.start_with? migration_task_prefix }
+end
+
+if migrating?
+  print "Skipping telegram update listener while migrating database\n"
+  return
+end
+
 Rails.application.config.after_initialize do
+  print "Initializing telegram update listener\n"
   TelegramUpdatePuller.instance.listen_all_channels
 end
